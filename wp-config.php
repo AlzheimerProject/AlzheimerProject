03c85d89c064f1bfa3fcded268014ec18f57d592
Théodore Cadet <cadetthe@gmail.com>
2021-12-18 00:02:42 +0000

    final touch

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'alzheimerproject' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'lol0000' );
+define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
